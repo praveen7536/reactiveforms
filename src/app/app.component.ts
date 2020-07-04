b483cca8ce6ec45003e6c6dffd601c994aec3cd5
Praveen <praveenkumar75369@gmail.com>
2020-07-05 00:55:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{ FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'userforms';
+  userforms;
+  userDetails = [];
+
+  constructor(private fb: FormBuilder) {
+
+    this.userforms = this.fb.group({
+      'name': this.fb.control('', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+      'email': this.fb.control('', [Validators.required, Validators.email]),
+      'dob': this.fb.control('', Validators.required),
+      'country': this.fb.control('IN'),
+      'gender': this.fb.control('', Validators.required),
+      'maritialstatus': this.fb.control('', Validators.required),
+      'favfood': this.fb.control(''),
+      'mobile': this.fb.control('', Validators.required),
+      'address': this.fb.array([
+        this.fb.group({
+          'street': this.fb.control('', [Validators.required]),
+          'doorno': this.fb.control('', [Validators.required]),
+          'zipcode': this.fb.control('', [Validators.required])
+        }),
+        this.fb.group({
+          'street': this.fb.control('', [Validators.required]),
+          'doorno': this.fb.control(''),
+          'zipcode': this.fb.control('')
+        }),
+        this.fb.group({
+          'street': this.fb.control('', [Validators.required]),
+          'doorno': this.fb.control(''),
+          'zipcode': this.fb.control('')
+        })
+      ]),
+    })
+  }
+  submitForm(){
+    console.log(this.userforms)
+    if(this.userforms.valid){
+       console.log(this.userforms.value);
+       this.userDetails.push(this.userforms.value);
+       console.log(this.userDetails);
+    }
+    console.log(this.userDetails)
+   }
+
+
 }
+
+
